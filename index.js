78147b48ec011704917f2449ab99fccf3106fa40
Dominik Tränklein <dominik@bleech.de>
2015-09-26 00:17:14 +0200

    switch to jade2php

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict';
 
-var jade = require('jade');
-var jadePhp = require('jade-php');
+var jadePhp = require('jade2php');
 var through = require('through2');
 var ext = require('gulp-util').replaceExtension;
 var PluginError = require('gulp-util').PluginError;
-
-jadePhp(jade);
+var merge = require('merge');
 
 function handleExtension(filepath, opts){
   return ext(filepath, '');
 }
 
 module.exports = function(options){
-  var opts = options || {};
+  var defaultOptions = {
+    omitPhpRuntime: true,
+    omitPhpExtractor: true
+  };
+  var opts = merge(defaultOptions, options);
 
   function CompileJade(file, enc, cb){
     opts.filename = file.path;
@@ -30,7 +32,7 @@ module.exports = function(options){
 
     if(file.isBuffer()){
       try {
-        file.contents = new Buffer(jade.render(String(file.contents), opts));
+        file.contents = new Buffer(jadePhp(String(file.contents), opts));
       } catch(e) {
         return cb(new PluginError('gulp-jade', e));
       }
